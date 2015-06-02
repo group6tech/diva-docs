888ea5a57cbd87cfaf51289757138949be2d0d11
Matt Dwen <matt@group6.co.nz>
2015-06-02 19:53:01 +1200

    Updated content 2-Jun-2015

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,7 +53,7 @@ module.exports = function (grunt) {
     connect: {
       options: {
         port: 9000,
-        open: 'http://localhost:9000/diva/index.htm',
+        open: 'http://localhost:9000/',
         livereload: 35729,
         // Change this to '0.0.0.0' to access the server from outside
         hostname: 'localhost'
