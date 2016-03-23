3191110a2fb01f6a7b29fc1d34c198cd4b6a8c87
Matt Dwen <matt@group6.co.nz>
2016-03-23 17:10:46 +1300

    Search nav

--- app/scripts/navbar.js
+++ app/scripts/navbar.js
@@ -3,6 +3,13 @@ $(function() {
     $('.navbar__content').toggleClass('is-closed').toggleClass('is-open');
   }
 
+  function navbarSearchToggle() {
+    $('.navbar__search').toggleClass('is-open');
+  }
+
   $('.navbar__toggler').click(function() { navbarContentToggle(); });
   $('.navbar__content__close').click(function() { navbarContentToggle(); });
+
+  $('.navbar__button--search').click(function() { navbarSearchToggle(); });
+  $('.navbar__search__close').click(function() { navbarSearchToggle(); });
 });
